45abbfd1b85a3f63198df2f36c9c6a5d6e72f6ae
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2023-09-08 10:51:17 +0200

    added new font

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className={"footer"}>Developed by @RMorenoDev</footer>
+    <footer className={"footer"}>Developed by RMorenoDev</footer>
   )
 }
 
